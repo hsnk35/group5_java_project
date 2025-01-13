49c77c29e39fdb31f72b9178eead28fceed19566
hsnk35 <hsnkllgl46@gmail.com>
2025-01-14 00:02:03 +0300

    proje created

--- src/hasankililioglu/week1/OddAndEven_hk.java
+++ src/hasankililioglu/week1/OddAndEven_hk.java
@@ -3,17 +3,18 @@ package hasankililioglu.week1;
 public class OddAndEven_hk {
     public static void main(String[] args) {
 
-       int num =99;
-       String result = identify(num);
-       System.out.println(result);
+      identify(99);
+
 
     }
 
-    public static String identify(int number) {
+    public static void identify(int number) {
         if (number % 2 == 0) {
-            return "even";
+            System.out.println("\"number\" = " + "even");
         }else {
-            return "odd";
+            System.out.println("\"number\" = " + "odd");
         }
+
+
     }
 }
