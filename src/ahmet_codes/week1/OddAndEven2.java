7b3b0d47654fade0dc5d74baf747bfd3dafa971f
ahmet <ahmett123@hotmail.com>
2025-01-14 20:59:37 +0300

    different codes for same task

--- src/ahmet_codes/week1/OddAndEven2.java
+++ src/ahmet_codes/week1/OddAndEven2.java
@@ -1,4 +1,26 @@
 package ahmet_codes.week1;
 
+import java.util.Scanner;
+
 public class OddAndEven2 {
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.print("Enter an integer number : ");
+
+        int number=scanner.nextInt();
+
+        if (number==0) {
+            System.out.println("Number is Zero");
+        }
+        else if (number%2==0) {
+            System.out.println(number+" number is Even");
+        }else
+        {
+            System.out.println(number+" number is Odd");
+        }
+
+
+    }
 }
