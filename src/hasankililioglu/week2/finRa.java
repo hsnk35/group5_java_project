9c55ee697d70f2b8c98cee71a2709d7b40600c11
hsnk35 <hsnkllgl46@gmail.com>
2025-01-21 18:42:07 +0300

    project created

---
+++ src/hasankililioglu/week2/finRa.java
@@ -0,0 +1,12 @@
+package hasankililioglu.week2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class oddOrEven {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30));
+        System.out.println(list);
+    }
+}
