17de755af5daf1476a5f108acbcb9302d59fc3ed
hsnk35 <hsnkllgl46@gmail.com>
2025-01-21 18:59:02 +0300

    project created

--- src/hasankililioglu/week2/finRa.java
+++ src/hasankililioglu/week2/finRa.java
@@ -1,12 +1,23 @@
 package hasankililioglu.week2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class oddOrEven {
+public class finRa {
     public static void main(String[] args) {
 
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30));
-        System.out.println(list);
+        for (int i = 1; i < 30; i++) {
+
+            if (i % 3 == 0) {
+                System.out.println("Fin");
+            }else if (i % 5 == 0) {
+                System.out.println("RA");
+            }else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FinRA");
+            }else {
+                System.out.println(i);
+            }
+
+        }
+
     }
 }
+
+
