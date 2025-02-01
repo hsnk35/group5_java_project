a40b7864cf36da79c79e0e82ac017101fdbf6b6a
ahmet <ahmett123@hotmail.com>
2025-02-01 23:39:12 +0300

    task week3 is created

--- src/ahmet_codes/week3/SwapNumbers.java
+++ src/ahmet_codes/week3/SwapNumbers.java
@@ -4,6 +4,18 @@ public class SwapNumbers {
     public static void main(String[] args) {
 
 
+        int a=21;
+        int b=22;
+        System.out.println("a is = "+a);
+        System.out.println("b is = "+b);
+
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a is = "+a);
+        System.out.println("b is = "+b);
+
     }
 
 }
