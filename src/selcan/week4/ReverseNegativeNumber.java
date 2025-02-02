5473f270d6cb4661f1416ff23e9c09e5fea53670
Sumeyra_S <selcanelisa@gmail.com>
2025-02-02 22:02:05 +0300

    Fourth Project

---
+++ src/selcan/week4/ReverseNegativeNumber.java
@@ -0,0 +1,40 @@
+package selcan.week4;
+
+public class ReverseNegativeNumber {
+    public static void main(String[] args) {
+
+        int num = 12345;
+        int resultNumber = reverse(num);
+        System.out.println(resultNumber); // Çıktı
+    }
+
+    private static int reverse(int number) {
+        int result = 0;
+
+        // Sayı negatif mi kontrol et
+        if (number < 0) {
+            number = -number; // Negatifse pozitife çevir
+        }
+
+        while (number > 0) {
+            result = result * 10 + number % 10;
+            number /= 10;
+        }
+
+        // Başta negatifse sonucu da negatif yap
+        if (result != 0) {
+            return -result;
+        }
+
+        return result;
+    }
+}
+
+
+
+
+
+
+
+
+
