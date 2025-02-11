e0e72fe52d085cc116027163e4ef47fdca903087
ahmet <ahmett123@hotmail.com>
2025-02-12 01:20:38 +0300

    some additions

--- src/ahmet_codes/week3/SwapNumbers.java
+++ src/ahmet_codes/week3/SwapNumbers.java
@@ -3,12 +3,10 @@ package ahmet_codes.week3;
 public class SwapNumbers {
     public static void main(String[] args) {
 
-
-        int a=21;
-        int b=22;
+        int a=20;
+        int b=10;
         System.out.println("a is = "+a);
         System.out.println("b is = "+b);
-
 
         a=a+b;
         b=a-b;
