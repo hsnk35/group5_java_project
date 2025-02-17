90dcd34f81ccf636600ad688743527c5457605ca
life <uchukeyeva@gmail.com>
2025-02-17 23:31:24 +0300

    Changes are made

--- src/salia/week4/ReverseNegativeNumber.java
+++ src/salia/week4/ReverseNegativeNumber.java
@@ -9,11 +9,14 @@ public class ReverseNegativeNumber {
 
     }
     public static int reverseNegativeNumber(int number) {
+        // -12345 ----> 54321-     -54321
         String str=""+number*-1;
+        //parse int into string, reverse it, and parse it back into int.
+
         StringBuilder sb=new StringBuilder(str);
         sb=sb.reverse();
-       int n= Integer.valueOf(sb.toString());
-        //int n=Integer.parseInt(sb.toString());
+        //int n= Integer.valueOf(sb.toString());
+        int n=Integer.parseInt(sb.toString());           // Convert the string to an integer
         n=n*(-1);
         return n;
     }
